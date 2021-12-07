3a2e2c3da2101ba7b7143fbefceaac2f85db44fe
Ekrem <ekrem@3pounds.io>
2021-12-07 16:56:24 +0300

    Foundation

---
+++ WeatherDemoApp/WeatherDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherDemoApp
+//
+//  Created by Ekrem Özkaraca on 7.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherDemoApp/WeatherDemoApp/WeatherDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoAppApp.swift
+//  WeatherDemoApp
+//
+//  Created by Ekrem Özkaraca on 7.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
