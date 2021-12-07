a3553ba796ee242e86e89b698c16230db4ec875a
Ekrem <ekrem@3pounds.io>
2021-12-07 18:57:03 +0300

    all ui complete.

--- WeatherDemoApp/WeatherDemoApp/Views/WeatherView.swift
+++ WeatherDemoApp/WeatherDemoApp/Views/WeatherView.swift
@@ -47,6 +47,34 @@ struct WeatherView: View {
             }
             .padding()
             .frame(maxWidth : .infinity, alignment: .leading)
+            
+            
+            VStack {
+                Spacer()
+                
+                VStack (alignment: .leading, spacing: 20) {
+                    Text("Weather Now : ")
+                        .bold().padding(.bottom)
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "Min temp", value: weather.main.tempMin.roundDouble() + "°")
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max temp", value: weather.main.tempMax.roundDouble() + "°")
+                    }
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind speed", value: weather.wind.speed.roundDouble() + "m/s")
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: weather.main.humidity.roundDouble() + "%")
+                    }
+                }.frame(maxWidth : .infinity, alignment: .leading)
+                    .padding()
+                    .padding(.bottom, 20)
+                    .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                    .background(.white)
+                    .cornerRadius(20, corners: [.topLeft, .topRight])
+                
+                
+                
+            }
         }
         .edgesIgnoringSafeArea(.bottom)
         .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
